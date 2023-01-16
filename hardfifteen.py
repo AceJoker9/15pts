7e1285389c511151855694ba30f8753c71f928b5
unknown <neilmagic25@gmail.com>
2023-01-16 01:32:01 -0500

    getting more comfortable

--- hardfifteen.py
+++ hardfifteen.py
@@ -11,8 +11,16 @@ entertianment = (random.choice(fun_time))
 
 wheels = ["jet", "viper", "hellcat", "infiniti coupe", "yukon",]
 transportation = (random.choice(wheels))
+round_trip = ["landscape:", "food:", "event:", "vehicle:"]
+
+def arrival(city):
+    good_choice = input("Do you want to travel here?")
+    for i in destination:
+        print(destination, good_choice)
+        if good_choice == "yes":
+            round_trip.append(destination[0])
+        return(destination)
 
 
-print(destination)
 
 
