acd9f9560088ef5ae4aef0acba7dcae2ebf9af85
unknown <neilmagic25@gmail.com>
2023-01-16 01:18:19 -0500

    still working with it

--- hardfifteen.py
+++ hardfifteen.py
@@ -3,6 +3,11 @@ import random
 location = ["oakland", "seattle", "san jose", "san diego", "boston", "detroit"]
 destination = (random.choice (location))
 
+eat_eat = [ "tacos", "seafood gallery", "coney island", "steakhouse", "roscos"]
+restuarant = (random.choice(eat_eat))
+
+
+
 print(destination)
 
 
