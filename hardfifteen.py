d48f8f1e28a9b7dfab910519cba42d04a6c8f0bc
unknown <neilmagic25@gmail.com>
2023-01-16 09:53:00 -0500

    I've ran into a strong halt

--- hardfifteen.py
+++ hardfifteen.py
@@ -12,14 +12,22 @@ entertianment = (random.choice(fun_time))
 wheels = ["jet", "viper", "hellcat", "infiniti coupe", "yukon",]
 transportation = (random.choice(wheels))
 round_trip = ["landscape:", "food:", "event:", "vehicle:"]
+good_choice = input("Do you want to travel here?")
 
-def arrival(city):
-    good_choice = input("Do you want to travel here?")
-    for i in destination:
+def arrival():
+    for city in destination:
         print(destination, good_choice)
         if good_choice == "yes":
             round_trip.append(destination[0])
+        else:
+            continue
+
         return(destination)
+    
+arrival()
+
+#Im attemping to perform a function where it will print a random city location and upon agreement, it will be added to a list
+
 
 
 
