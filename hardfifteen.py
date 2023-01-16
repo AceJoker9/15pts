c8aeb565021c6e99fedef392834c13e5ca3e1d89
unknown <neilmagic25@gmail.com>
2023-01-16 01:23:56 -0500

    positive energy flowing right now

--- hardfifteen.py
+++ hardfifteen.py
@@ -6,6 +6,11 @@ destination = (random.choice (location))
 eat_eat = [ "tacos", "seafood gallery", "coney island", "steakhouse", "roscos"]
 restuarant = (random.choice(eat_eat))
 
+fun_time = ["paintball", "movies", "naked wrestling", "bowling", "hookah"]
+entertianment = (random.choice(fun_time))
+
+wheels = ["jet", "viper", "hellcat", "infiniti coupe", "yukon",]
+transportation = (random.choice(wheels))
 
 
 print(destination)
