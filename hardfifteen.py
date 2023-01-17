57e80b271e0b69c30f3005354d0e7a49eb22e557
unknown <neilmagic25@gmail.com>
2023-01-17 01:59:26 -0500

    Finished the second function

--- hardfifteen.py
+++ hardfifteen.py
@@ -3,7 +3,7 @@ import random
 location = ["oakland", "seattle", "san jose", "san diego", "boston", "detroit"]
 destination = random.choice(location)
 
-eat_eat = [ "tacos", "seafood gallery", "coney island", "steakhouse", "roscos"]
+eat_eat = ["tacos", "seafood gallery", "coney island", "steakhouse", "roscos"]
 restuarant = (random.choice(eat_eat))
 
 fun_time = ["paintball", "movies", "naked wrestling", "bowling", "hookah"]
@@ -36,6 +36,25 @@ arrival()
 
 
 #Im attemping to perform a function where itye will print a random city location and upon agreement, it will be added to a list
+
+
+def dinner():
+    isHappy = False
+    while isHappy == False:
+        restuarant = (random.choice(eat_eat))
+        print(f"Your date is {restuarant}")
+        good_date = input("Do you want to eat here?")
+        if good_date == "yes":
+            round_trip.append(restuarant)
+            print("Good Date Idea", round_trip)
+            isHappy = True
+        else:
+            continue
+
+        return(restuarant)
+    
+dinner()
+
 
 
 
